4b0ed610518a1b71f12f7309f2c9d57d5e2cff01
Clara Jordan <clara.jordan@hpc.tools>
2022-05-23 13:26:26 +0100

    Remove switch language button on logged out page

--- apps/hpc-ftsadmin-site/src/app/pages/not-logged-in.tsx
+++ apps/hpc-ftsadmin-site/src/app/pages/not-logged-in.tsx
@@ -1,12 +1,5 @@
-import React from 'react';
-import { MdTranslate } from 'react-icons/md';
-
 import { C, CLASSES, styled } from '@unocha/hpc-ui';
-
-import { t, LANGUAGE_CHOICE } from '../../i18n';
-import en from '../../i18n/langs/en';
-import fr from '../../i18n/langs/fr';
-
+import { t } from '../../i18n';
 import { AppContext } from '../context';
 
 interface Props {
@@ -21,10 +14,6 @@ const Container = styled.div`
   padding-bottom: 40px;
 `;
 
-const Actions = styled.div`
-  text-align: center;
-`;
-
 const AUN = styled(C.AcceptableUseNotification)`
   flex-grow: 1;
 `;
@@ -33,24 +22,6 @@ const PageNotLoggedIn = (props: Props) => (
   <AppContext.Consumer>
     {({ lang, env }) => (
       <Container className={CLASSES.CONTAINER.CENTERED}>
-        <Actions>
-          {lang !== 'en' && (
-            <C.Button
-              startIcon={MdTranslate}
-              onClick={() => LANGUAGE_CHOICE.setLanguage('en')}
-              color="secondary"
-              text={en.strings.switchToThisLanguage}
-            />
-          )}
-          {lang !== 'fr' && (
-            <C.Button
-              startIcon={MdTranslate}
-              onClick={() => LANGUAGE_CHOICE.setLanguage('fr')}
-              color="secondary"
-              text={fr.strings.switchToThisLanguage}
-            />
-          )}
-        </Actions>
         <AUN
           session={env().session}
           strings={t.get(lang, (s) => s.components.acceptableUseNotification)}
